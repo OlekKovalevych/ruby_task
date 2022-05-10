63c94f12e26b909764b68e2b50cd294cb51beda5
OlekKovalevych <kovalevich.148@gmail.com>
2022-05-10 15:20:00 +0200

    Sort

--- sort/lib/sort.rb
+++ sort/lib/sort.rb
@@ -1,2 +1,14 @@
 module Sort
+  def bubble_sort(array, index = 0)
+    (0...array.size).each do
+      (0...array.size - 1).each do |j|
+        next unless array[j] > array[j + 1]
+
+        temp = array[j]
+        array[j] = array[j + 1]
+        array[j + 1] = temp
+      end
+    end
+    array
+  end
 end
--- sort/main_sort.rb
+++ sort/main_sort.rb
@@ -1,1 +1,14 @@
+require_relative "lib/sort"
 
+include Sort
+
+array = []
+
+20.times do
+  array.append(rand(0...200))
+end
+puts "Default array"
+print array
+puts
+puts "Sort array"
+print Sort.bubble_sort(array)
